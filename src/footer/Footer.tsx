6774201eb478d8c105428b25a4a0c6b11c9b7e35
ricardoaguiarvieira <ricardoaguiarfbc@gmail.com>
2021-01-20 17:34:07 -0300

    Update margin manu and code smells

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -9,16 +9,16 @@ function Footer() {
         <div className="row footer-bar">
             <div className="col">
                 <div className="links">
-                    <a href="https://www.instagram.com/tatyaraujo9/?hl=pt-br"  target="_blank">
+                    <a href="https://www.instagram.com/tatyaraujo9/?hl=pt-br" target="_blank" rel="noreferrer">
                         <Insta />
                     </a>
-                    <a href="https://api.whatsapp.com/send?phone=5561994419212"  target="_blank">
+                    <a href="https://api.whatsapp.com/send?phone=5561994419212"  target="_blank" rel="noreferrer">
                         <Whats />
                     </a>
-                    <a href="https://www.tiktok.com/@tatycuesta?lang=pt-BR"  target="_blank">
+                    <a href="https://www.tiktok.com/@tatycuesta?lang=pt-BR"  target="_blank" rel="noreferrer">
                         <SiTiktok />
                     </a>
-                    <a href="https://twitter.com/Tatirodrigues9_?s=09"  target="_blank">
+                    <a href="https://twitter.com/Tatirodrigues9_?s=09"  target="_blank" rel="noreferrer">
                         <Twitter />
                     </a>
                 </div>
@@ -26,8 +26,8 @@ function Footer() {
                     <span>tatyarauj5@gmail.com</span>
                 </div>
                 <div className="creat">
-                    <span> create by: <a href="https://www.instagram.com/ricardoaguiarfbc" target="_blank">
-                        @Ricardo Aguiar <Insta /></a>
+                    <span> create by: <a href="https://www.instagram.com/ricardoaguiarfbc" target="_blank" rel="noreferrer">
+                        Ricardo Aguiar <Insta /></a>
                     </span>
                 </div>
             </div>
